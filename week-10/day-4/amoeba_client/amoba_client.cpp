0e94d646b40a9e367d4fbe8fd1cd0784b3e13a56
Balazs Makszim <b.makszim@hotmail.com>
2017-01-05 12:27:43 +0100

    amoeba server and client added

---
+++ week-10/day-4/amoeba_client/amoba_client.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_net.h>
+#include <string>
+#include <vector>
+#include "draw.hpp"
+#include "game-logic.hpp"
+
+using namespace std;
+
+void _draw(vector<vector<char>> v);
+
+int main(int argc, char ** argv) {
+  Game game;
+  int width = 570;
+  int height = 570;
+  bool quit = false;
+  SDL_Event event;
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  /////////////////SDL_NET///////////////
+  SDLNet_Init();
+  IPaddress ip;
+  SDLNet_SocketSet set;
+  set = SDLNet_AllocSocketSet(2);
+  SDLNet_ResolveHost(&ip, "127.0.0.1", 1234);
+  TCPsocket client = SDLNet_TCP_Open(&ip);
+  SDLNet_TCP_AddSocket(set, client);
+  /////////////////SDL_NET///////////////
+
+  SDL_Window* window = SDL_CreateWindow("SDL2 Amoba",
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_SetRenderDrawColor(renderer, 220, 220, 255, 0);
+  SDL_RenderClear(renderer);
+  vector<vector<char>> v;
+  while (!quit) {
+
+    v = game.get_vector();
+    SDL_WaitEvent(&event);
+    if (event.type == SDL_QUIT) {
+      quit = true;
+      break;
+    }
+    if (event.type == SDL_KEYDOWN) {
+      if (event.key.keysym.sym == SDLK_q) {
+        quit = true;
+      }
+    }
+    if (!game.get_next()) {
+    if (event.type == SDL_MOUSEBUTTONDOWN) {
+      int _x;
+      int _y;
+      SDL_GetMouseState(&_x, &_y);
+      if (!game.is_won()) {
+        game.set_cell((_x / (width / 19)), (_y / (height / 19)));
+        //SDLnet
+        int array[2] = {(_x / (width / 19)), (_y / (height / 19))};
+        SDLNet_TCP_Send(client, &array, 2 * sizeof(int));
+
+        //SDLnet
+        _draw(game.get_vector());
+      }
+      cout << game.is_won() << endl;
+    }
+    } else {
+      int _x;
+      int _y;
+      int array[2];
+
+      int activeSockets = SDLNet_CheckSockets(set, 10);
+      if (activeSockets != 0) {
+        int gotMessage = SDLNet_SocketReady(client);
+        if (gotMessage != 0) {
+          SDLNet_TCP_Recv(client, &array, 100);
+          _x = array[0];
+          _y = array[1];
+
+          if (!game.is_won()) {
+            game.set_cell(_x, _y);
+            _draw(game.get_vector());
+          }
+          cout << game.is_won() << endl;
+        }
+      }
+    }
+    v = game.get_vector();
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = 1; i < 19; i++) {
+      SDL_RenderDrawLine(renderer, i * (width / 19), 0, i * (width / 19), height);
+      SDL_RenderDrawLine(renderer, 0, i * (height / 19), width, i * (height / 19));
+    }
+    for (int i = 0; i < 19; i++) {
+      for (int j = 0; j < 19; j++) {
+        if (v[j][i] == 'X') {
+          SDL_RenderDrawLine(renderer, i * (width / 19), j * (height / 19), (i + 1) * (width / 19), (j + 1) * (height / 19));
+          SDL_RenderDrawLine(renderer, (i + 1) * (width / 19), j * (height / 19), i * (width / 19), (j + 1) * (height / 19));
+        }
+        if (v[j][i] == 'O') {
+          SDL_RenderDrawLine(renderer, i * (width / 19) + 6, j * (height / 19) + 6, i * (width / 19) + 6, j * (height / 19) + 24);
+          SDL_RenderDrawLine(renderer, i * (width / 19) + 24, j * (height / 19) + 6, i * (width / 19) + 24, j * (height / 19) + 24);
+          SDL_RenderDrawLine(renderer, i * (width / 19) + 6, j * (height / 19) + 6, i * (width / 19) + 24, j * (height / 19) + 6);
+          SDL_RenderDrawLine(renderer, i * (width / 19) + 6, j * (height / 19) + 24, i * (width / 19) + 24, j * (height / 19) + 24);
+        }
+        if (v[j][i] == 'X' || v[j][i] == 'O') {
+          if (game.is_5in_lineh(j, i)) {
+            SDL_RenderDrawLine(renderer, i * (width / 19) + 15, j * (width / 19) + 15, i * (width / 19) + 15, (j + 4) * (width / 19) + 15);
+          }
+        }
+        if (v[j][i] == 'X' || v[j][i] == 'O') {
+          if (game.is_5in_linev(j, i)) {
+            SDL_RenderDrawLine(renderer, i * (width / 19) + 15, j * (width / 19) + 15, (i + 4) * (width / 19) + 15, j * (width / 19) + 15);
+          }
+        }
+        if (v[j][i] == 'X' || v[j][i] == 'O') {
+          if (game.is_5in_linec1(j, i)) {
+            SDL_RenderDrawLine(renderer, i * (width / 19) + 15, j * (width / 19) + 15, (i + 4) * (width / 19) + 15, (j + 4) * (width / 19) + 15);
+          }
+        }
+        if (v[j][i] == 'X' || v[j][i] == 'O') {
+          if (game.is_5in_linec2(j, i)) {
+            SDL_RenderDrawLine(renderer, i * (width / 19) + 15, j * (width / 19) + 15, (i - 4) * (width / 19) + 15, (j + 4) * (width / 19) + 15);
+          }
+        }
+      }
+    }
+    SDL_RenderPresent(renderer);
+    //SDLnet
+
+    //SDLnet
+  }
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
+
+void _draw(vector<vector<char>> v) {
+  char c = 65;
+  for (int i = 0; i < 19; i++) {
+    cout << c++ << '|';
+  }
+  cout << endl;
+  for (unsigned int i = 0; i < v.size(); i++) {
+    for (unsigned int j = 0; j < v[i].size(); j++) {
+      cout << v[i][j] << '|';
+    }
+    cout << i << endl;
+  }
+}
---
+++ week-10/day-4/amoeba_client/game-logic.cpp
@@ -0,0 +1,99 @@
+#include "game-logic.hpp"
+
+Game::Game() {
+  vector<char> row;
+  for (int i = 0; i < 19; i++) {
+    row.push_back('_');
+  }
+  for (int i = 0; i < 19; i++) {
+    v.push_back(row);
+  }
+  x_next = false;
+}
+
+vector<vector<char>> Game::get_vector() {
+  return v;
+}
+
+void Game::set_cell(unsigned int x, unsigned int y) {
+  if (v[y][x] != 'X' && v[y][x] != 'O') {
+    if (x_next) {
+      v[y][x] = 'X';
+    } else {
+      v[y][x] = 'O';
+    }
+    x_next = !x_next;
+  }
+}
+
+bool Game::is_5in_lineh(unsigned int _x, unsigned int _y) {
+  int x = _x;
+  int y = _y;
+  int i = 1;
+  if (x < 15) {
+    while (i < 5 && v[x][y] == v[x + 1][y]) {
+      i++;
+      x++;
+    }
+  }
+  return (i >= 5);
+}
+
+bool Game::is_5in_linev(unsigned int _x, unsigned int _y) {
+  int x = _x;
+  int y = _y;
+  int j = 1;
+  if (y < 15) {
+    while (j < 5 && v[x][y] == v[x][y + 1]) {
+      j++;
+      y++;
+    }
+  }
+  return (j >= 5);
+}
+
+bool Game::is_5in_linec1(unsigned int _x, unsigned int _y) {
+  int x = _x;
+  int y = _y;
+  int k = 1;
+  if (x < 15 && y < 15) {
+    while (k < 5 && v[x][y] == v[x + 1][y + 1]) {
+      k++;
+      x++;
+      y++;
+    }
+  }
+  return (k >= 5);
+}
+
+bool Game::is_5in_linec2(unsigned int _x, unsigned int _y) {
+  int x = _x;
+  int y = _y;
+  int l = 1;
+  if (x < 15 && y > 3) {
+    while (l < 5 && v[x][y] == v[x + 1][y - 1]) {
+      l++;
+      x++;
+      y--;
+    }
+  }
+  return (l >= 5);
+}
+
+bool Game::is_won() {
+  bool won = false;
+  for (unsigned int i = 0; i < v.size(); i++) {
+    for (unsigned int j = 0; j < v[i].size(); j++) {
+      if (v[i][j] == 'X' || v[i][j] == 'O') {
+        if ((is_5in_lineh(i, j)) || (is_5in_linev(i, j)) || (is_5in_linec1(i, j)) || (is_5in_linec2(i, j))) {
+          won = true;
+        }
+      }
+    }
+  }
+  return won;
+}
+
+bool Game::get_next() {
+  return x_next;
+}
