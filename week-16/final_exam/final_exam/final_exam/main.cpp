e77a7fd241cb2fb6b65a99ab97bf11117f3bfcc3
Balazs Makszim <b.makszim@hotmail.com>
2017-02-17 09:57:06 +0100

    refactor: removes unnecessary cout in main function

--- week-16/final_exam/final_exam/final_exam/main.cpp
+++ week-16/final_exam/final_exam/final_exam/main.cpp
@@ -6,11 +6,6 @@ using namespace std;
 list<unsigned int> my_function(string _string, char _char);
 
 int main() {
-  list<unsigned int> my_list = my_function("Greenfox Academy", 'E');
-  for (list<unsigned int>::iterator it = my_list.begin(); it != my_list.end(); it++) {
-    cout << *it << ' ';
-  }
-  cout << endl;
   return 0;
 }
 
