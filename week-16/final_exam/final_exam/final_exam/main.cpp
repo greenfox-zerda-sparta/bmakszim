a4266fe469a9200a07d614014d6a1060445651a3
Balazs Makszim <b.makszim@hotmail.com>
2017-02-17 09:38:07 +0100

    adds final exam exercise

---
+++ week-16/final_exam/final_exam/final_exam/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+list<unsigned int> my_function(string _string, char _char);
+
+int main() {
+  list<unsigned int> my_list = my_function("zazu", 'z');
+  for (list<unsigned int>::iterator it = my_list.begin(); it != my_list.end(); it++) {
+    cout << *it << ' ';
+  }
+  cout << endl;
+  return 0;
+}
+
+list<unsigned int> my_function(string _string, char _char) {
+  list<unsigned int> _list;
+  for (unsigned int i = 0; i < _string.length(); i++) {
+    if (_string[i] == _char) {
+      _list.push_back(i);
+    }
+  }
+  return _list;
+}
